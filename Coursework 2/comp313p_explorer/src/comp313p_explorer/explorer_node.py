278edcb4a59287e3c1619341cc982b2d6b7ca01e
Horatiu Ilie <ilie.horatiu@yahoo.com>
2018-04-19 17:10:06 +0300

    indent + updateFrontier()

--- Coursework 2/comp313p_explorer/src/comp313p_explorer/explorer_node.py
+++ Coursework 2/comp313p_explorer/src/comp313p_explorer/explorer_node.py
@@ -12,66 +12,76 @@ class ExplorerNode(ExplorerNodeBase):
         ExplorerNodeBase.__init__(self)
 
         self.blackList = []
-	self.currPos = None
+    self.currPos = None
 
+    # returns false if nor more frontiers
     def updateFrontiers(self):
-        pass
+        for x in range(0, self.occupancyGrid.getWidthInCells()):
+            for y in range(0, self.occupancyGrid.getHeightInCells()):
+                candidate = (x, y)
+                if self.isFrontierCell(x, y) is True:
+                    candidateGood = True
+                    for k in range(0, len(self.blackList)):
+                        if self.blackList[k] == candidate:
+                            candidateGood = False
+                            break
+
+                    if candidateGood is True:
+                        return True
+                        
+        return False
 
     def chooseNewDestination(self):
+        if(self.currPos is None):
+            for x in range(0, self.occupancyGrid.getWidthInCells()):
+                for y in range(0, self.occupancyGrid.getHeightInCells()):
+                    candidate = (x, y)
+                    if self.isFrontierCell(x, y) is True:
+                        candidateGood = True
+                        for k in range(0, len(self.blackList)):
+                            if self.blackList[k] == candidate:
+                                candidateGood = False
+                                break
 
+                        if candidateGood is True:
+                            self.currPos = candidate
+                            return True, candidate
+            
+            self.currPos = None
+            return False, None
 
-#         print 'blackList:'
-#         for coords in self.blackList:
-#             print str(coords)
+        else:
+            minVal = 2000000.0
+            nextCandidate = None
 
-	if(self.currPos is None):
-		for x in range(0, self.occupancyGrid.getWidthInCells()):
-		    for y in range(0, self.occupancyGrid.getHeightInCells()):
-		        candidate = (x, y)
-		        if self.isFrontierCell(x, y) is True:
-		            candidateGood = True
-		            for k in range(0, len(self.blackList)):
-		                if self.blackList[k] == candidate:
-		                    candidateGood = False
-		                    break
+            for x in range(0, self.occupancyGrid.getWidthInCells()):
+                for y in range(0, self.occupancyGrid.getHeightInCells()):
+                    candidate = (x, y)
+                    if self.isFrontierCell(x, y) is True:
+                        candidateGood = True
+                        for k in range(0, len(self.blackList)):
+                            if self.blackList[k] == candidate:
+                                candidateGood = False
+                                break
 
-		            if candidateGood is True:
-				        self.currPos = candidate
-		                return True, candidate
-		
-		self.currPos = None
-		return False, None
+                        if candidateGood is True:
+                            distanceToCandidate = math.sqrt((candidate[0] - self.currPos[0]) ** 2 + (candidate[1] - self.currPos[1]) ** 2)
+                            if distanceToCandidate < minVal:
+                               minVal = distanceToCandidate
+                               nextCandidate = candidate
+            
+            self.currPos = nextCandidate        
 
-	else:
-		minVal = 2000000.0
-		nextCandidate = None
-
-		for x in range(0, self.occupancyGrid.getWidthInCells()):
-		    for y in range(0, self.occupancyGrid.getHeightInCells()):
-		        candidate = (x, y)
-		        if self.isFrontierCell(x, y) is True:
-		            candidateGood = True
-		            for k in range(0, len(self.blackList)):
-		                if self.blackList[k] == candidate:
-		                    candidateGood = False
-		                    break
-
-		            if candidateGood is True:
-				        distanceToCandidate = math.sqrt((candidate[0] - self.currPos[0]) ** 2 + (candidate[1] - self.currPos[1]) ** 2)
-				        if distanceToCandidate < minVal:
-					       minVal = distanceToCandidate
-					       nextCandidate = candidate
-		
-		self.currPos = nextCandidate		
-
-		if(nextCandidate is not None):
-			return True, nextCandidate
-		else:
-			return False, None
-		
+            if(nextCandidate is not None):
+                return True, nextCandidate
+            else:
+                return False, None
+            
 
     def destinationReached(self, goal, goalReached):
         if goalReached is False:
 #             print 'Adding ' + str(goal) + ' to the naughty step'
             self.blackList.append(goal)
+            
+
             
