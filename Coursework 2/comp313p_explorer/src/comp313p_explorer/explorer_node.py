7e28a94280dd53ddebeed919ce72fcd0c9dc5c4b
Horatiu Ilie <ilie.horatiu@yahoo.com>
2018-04-19 19:06:50 +0300

    test test test

--- Coursework 2/comp313p_explorer/src/comp313p_explorer/explorer_node.py
+++ Coursework 2/comp313p_explorer/src/comp313p_explorer/explorer_node.py
@@ -14,6 +14,26 @@ class ExplorerNode(ExplorerNodeBase):
 	self.blackList = []
     	self.currPos = None
 
+
+    def getCandidateInSegmentation(self):
+        currCandidate = self.currPos
+
+        if(self.currPos is None):
+            return None
+        
+        row_sz = self.occupancyGrid.getWidthInCells() / 6
+        col_sz = self.occupancyGrid.getHeightInCells() / 4
+
+        for i in range(0, row_sz):
+            for j in range(0, col_sz):
+                if((i * row_sz) <= currCandidate[0] and ((i + 1) * row_sz) > currCandidate[0] and (j * col_sz) <= currCandidate[1] and ((j + 1) * col_sz) <= currCandidate[1]):
+                    return ( i * row_sz, (i + 1) * row_sz,  j * col_sz, (j + 1) * col_sz )
+
+        return None
+
+                        
+
+
     # returns false if nor more frontiers
     def updateFrontiers(self):        
 	for x in range(0, self.occupancyGrid.getWidthInCells()):
@@ -53,8 +73,29 @@ class ExplorerNode(ExplorerNodeBase):
             return False, None
 
         else:
+            coords = self.getCandidateInSegmentation()
             minVal = 2000000.0
             nextCandidate = None
+
+            for x in range(coords[0], coords[1]):
+                for y in range(coords[2], coords[3]):
+                    candidate = (x, y)
+                    if self.isFrontierCell(x, y) is True:
+                        candidateGood = True
+                        for k in range(0, len(self.blackList)):
+                            if self.blackList[k] == candidate:
+                                candidateGood = False
+                                break
+
+                        if candidateGood is True:
+                            distanceToCandidate = math.sqrt((candidate[0] - self.currPos[0]) ** 2 + (candidate[1] - self.currPos[1]) ** 2)
+                            if distanceToCandidate < minVal:
+                               minVal = distanceToCandidate
+                               nextCandidate = candidate
+
+            if(nextCandidate is not None):
+                self.currPos = nextCandidate
+                return True, nextCandidate
 
             for x in range(0, self.occupancyGrid.getWidthInCells()):
                 for y in range(0, self.occupancyGrid.getHeightInCells()):
