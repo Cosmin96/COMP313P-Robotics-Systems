63ddef25f54813677e8baaf4c9949c0379f0a38b
Horatiu Ilie <ilie.horatiu@yahoo.com>
2018-04-19 17:02:26 +0300

    fix indent

--- Coursework 2/comp313p_explorer/src/comp313p_explorer/explorer_node.py
+++ Coursework 2/comp313p_explorer/src/comp313p_explorer/explorer_node.py
@@ -25,7 +25,6 @@ class ExplorerNode(ExplorerNodeBase):
 #             print str(coords)
 
 	if(self.currPos is None):
-
 		for x in range(0, self.occupancyGrid.getWidthInCells()):
 		    for y in range(0, self.occupancyGrid.getHeightInCells()):
 		        candidate = (x, y)
@@ -37,14 +36,16 @@ class ExplorerNode(ExplorerNodeBase):
 		                    break
 
 		            if candidateGood is True:
-				self.currPos = candidate
+				        self.currPos = candidate
 		                return True, candidate
 		
 		self.currPos = None
 		return False, None
+
 	else:
 		minVal = 2000000.0
 		nextCandidate = None
+
 		for x in range(0, self.occupancyGrid.getWidthInCells()):
 		    for y in range(0, self.occupancyGrid.getHeightInCells()):
 		        candidate = (x, y)
@@ -56,11 +57,10 @@ class ExplorerNode(ExplorerNodeBase):
 		                    break
 
 		            if candidateGood is True:
-				
-				distanceToCandidate = math.sqrt((candidate[0] - self.currPos[0]) ** 2 + (candidate[1] - self.currPos[1]) ** 2)
-				if distanceToCandidate < minVal:
-					minVal = distanceToCandidate
-					nextCandidate = candidate
+				        distanceToCandidate = math.sqrt((candidate[0] - self.currPos[0]) ** 2 + (candidate[1] - self.currPos[1]) ** 2)
+				        if distanceToCandidate < minVal:
+					       minVal = distanceToCandidate
+					       nextCandidate = candidate
 		
 		self.currPos = nextCandidate		
 
